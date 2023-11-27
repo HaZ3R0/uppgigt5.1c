47299b0faf1c0084b8a9085e4641a23465079446
HaZ3R0 <megamackan@outlook.com>
2023-11-27 20:56:52 +0100

    Add project files.

---
+++ uppgigt5.1c/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift5._1c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många meningar vill du skriva?");
+            string antaltext = Console.ReadLine();
+            int antal = int.Parse( antaltext );
+            string[] mening = new string[antal];
+            for (int i = 0; i < antal; i++)
+            {
+               Console.WriteLine("Skriv en mening");
+                mening[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Här är de meningar du skrev");
+            for (int i = 0;i < antal; i++)
+            {
+                Console.WriteLine(mening[i]);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
